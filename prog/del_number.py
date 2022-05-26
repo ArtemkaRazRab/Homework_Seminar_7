5beb8bdfbbab5a42c8832fbe5b91e86acca9a796
«Artemka» <sergeev_artem91@mail.ru>
2022-05-26 17:06:07 +0500

    Homework seminar 7

--- prog/del_number.py
+++ prog/del_number.py
@@ -1,9 +1,9 @@
 def del_phone_number(contact):
     f = open(r'telephon_book.txt', 'r')
-    Line_new = f.readlines()
+    line_new = f.readlines()
     f.close()
     f = open(r'telephon_book.txt', 'w')
-    for line in Line_new: 
+    for line in line_new: 
         if line.find(contact) == -1:
             f.write(line)
     f.close()
